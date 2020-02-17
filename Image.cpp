341e21db126261e1bd28b54272172d4cbc02850d
HarryEvs21 <56265991+HarryEvs21@users.noreply.github.com>
2020-02-17 12:55:59 +0000

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -5,7 +5,44 @@ void Image::CheckCompatible() {
 	throw "Not yet implemented";
 }
 
+int Image::convertToAscii(string name){
+	
+	for (int i = 0; i < name.length(); i++){
+        	cout << (int)name[i]<< endl;
+		int nameNum = nameNum.append((int)name[i]);
+	return nameNum	
+    	}
+}
+
 void Image::SortAscendingName() {
+	
+	//Convert string to ascii
+	//Sort data
+	//Convert back to string
+	
+	//Array of Names to sort
+	nameArray =[];
+	numArray = [];
+	for(int i = 0; i < names.length(); i++){
+		numArray[i] = convertToAscii(nameArray[i]);
+	}
+	
+	//When numArray finished
+	//Sort the array into ascedning order
+	int temp;
+	for(i=0;i<names.length();i++){
+		for(j=i+1;j<nameArray.length();j++){
+			if(numArray[i]>numArray[j])
+			{
+				temp = numArray[i];
+				numArray[i] = numArray[j];
+				numArray[j] = temp;
+			}
+		}
+	}
+	
+	//Array is sorted so convert back to string
+	
 	// TODO - implement Image::SortAscendingName
 	throw "Not yet implemented";
 }
