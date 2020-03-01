5c6d49572df1bccf38c77c8fd1b079289be73991
HarryEvs21 <56265991+HarryEvs21@users.noreply.github.com>
2020-03-01 19:38:18 +0000

    Added TESTS FOLDER
    timer test works but the rest don't

--- QTPracticeNew/QTPracticeNew/Polygon.cpp
+++ QTPracticeNew/QTPracticeNew/Polygon.cpp
@@ -4,12 +4,12 @@ int vertices;
 
 //Mininum number of points???
 
-void Polygon::CheckMax8() {
+//void Polygon::CheckMax8() {
 	// TODO - implement Polygon::CheckMax8
-	while(vertices > 8 or vertices < 3){
-		cout<<"Invalid number of vertices, please enter in the range of 3-8"<<endl;
-		cout<<"Enter: ";
-		cin>>vertcies>>endl;
-	}
+    //while(vertices > 8 or vertices < 3){
+        //cout<<"Invalid number of vertices, please enter in the range of 3-8"<<endl;
+        //cout<<"Enter: ";
+        //cin>>vertcies>>endl;
+    //}
 	//throw "Not yet implemented";
-}
+//}
