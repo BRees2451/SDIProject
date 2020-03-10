7f137e313935891d16a624703f60ccef795d7d4a
BRees2451 <56265913+BRees2451@users.noreply.github.com>
2020-03-10 10:26:11 +0000

    Add files via upload

---
+++ SidePane.cpp
@@ -0,0 +1,36 @@
+#include "SidePane.h"
+
+void SidePane::DisplayClasses() {
+	// TODO - implement SIdePane::DisplayClasses
+
+    //Will trigger if new image is clicked
+
+	throw "Not yet implemented";
+}
+
+void SidePane::DisplayImages() {
+    //Image image = new Image();
+
+    //Get that list from mainwindow.h
+
+
+	throw "Not yet implemented";
+}
+
+void SidePane::getClasses() {
+    /*/////////////////////////////////////////Difference between getting and displaying ??
+     * retrieve list from mainwindow*/
+
+}
+
+void SidePane::displayClasses() {
+    /* get list
+     * run through list
+     * display list to classes pannel on gui
+     * */
+
+}
+
+void SidePane::addToVector(){
+
+}
---
+++ UserShapeOperations.cpp
@@ -0,0 +1,54 @@
+#include "UserShapeOperations.h"
+
+void UserShapeOperation::Size() {
+	// TODO - implement UerShapeOpperations::Size
+    /*
+     *
+     *
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::Move() {
+	// TODO - implement UerShapeOpperations::Move
+    /* list shapes
+     * select shape with mouse
+     * return selected shape index
+     * track mouse movements while mousepressed == true
+     * when mouse pressed == false retrieve coordiantes
+     * place shape at new coordiantes
+     * edit shap coordinates in list
+     *
+     *
+     * //////// mouse posistion on shape stays the same or ato implements to centre of shape ??????????????
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::Delete() {
+	// TODO - implement UerShapeOpperations::Delete
+    /* list shapes
+     * select shape
+     * return selected shape index
+     * enter shape list at index
+     * delete shape at index
+     *
+     *
+     * //////////////////ask for confirmation????
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::DisplayName() {
+	// TODO - implement UerShapeOpperations::DisplayName
+    /* list shapes
+     * select shape
+     * return shape index
+     * enter list at shape index
+     * return name of shape
+     * display name of shape on gui
+     *
+     *
+     * */
+	throw "Not yet implemented";
+}
