af546e9d9be3797dd53d5df68d462baae4c24b08
HarryEvs21 <56265991+HarryEvs21@users.noreply.github.com>
2020-03-26 18:22:19 +0000

    Merge branch 'master' of https://github.com/BRees2451/SDIProject

---
+++ SDIProject-master/SDIProject-master/QTPracticeNew/UserShapeOperations.cpp
@@ -0,0 +1,73 @@
+#include "UserShapeOperations.h"
+
+void UserShapeOperation::Size() {
+    /* set width
+     * set height
+     *
+     * if user input recieved{
+     * change weidth
+     * change height }
+     * else {width = 500;
+     *  height = 500;}
+     *
+     * to be called when drawshape is clicekd
+     * */
+
+    //option 1
+    /* list shapes
+     * select shape with mouse
+     * return selected shape index
+     * enter shape index
+     * enter shapesize
+     * edit shapesize
+     * exit shape
+     *
+     *
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::Move() {
+	// TODO - implement UerShapeOpperations::Move
+    /* list shapes
+     * select shape with mouse
+     * return selected shape index
+     * track mouse movements while mousepressed == true
+     * when mouse pressed == false retrieve coordiantes
+     * place shape at new coordiantes
+     * edit shape coordinates in list
+     * exit shape
+     *
+     *
+     * //////// mouse posistion on shape stays the same or ato implements to centre of shape ??????????????
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::Delete() {
+	// TODO - implement UerShapeOpperations::Delete
+    /* list shapes
+     * select shape
+     * return selected shape index
+     * enter shape list at index
+     * delete shape at index
+     *
+     *
+     * //////////////////ask for confirmation????
+     * */
+	throw "Not yet implemented";
+}
+
+void UserShapeOperation::DisplayName() {
+	// TODO - implement UerShapeOpperations::DisplayName
+    /* list shapes
+     * select shape
+     * return shape index
+     * enter list at shape index
+     * return name of shape
+     * display name of shape on gui ///////// cout<< shape[i].name
+     *
+     *
+     * */
+	throw "Not yet implemented";
+}
