8d780dab8de9cdac13a95b21bb3a09aa354f0273
MGibbs22 <56265981+MGibbs22@users.noreply.github.com>
2020-04-02 17:15:31 +0100

    BUILD

---
+++ SDIProject-master/BUILD/debug/moc_mainwindow.cpp
@@ -0,0 +1,202 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../QTPracticeNew/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[26];
+    char stringdata0[514];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 23), // "on_ZoomInButton_clicked"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 24), // "on_ZoomOutButton_clicked"
+QT_MOC_LITERAL(4, 61, 23), // "on_actionOpen_triggered"
+QT_MOC_LITERAL(5, 85, 23), // "on_actionQuit_triggered"
+QT_MOC_LITERAL(6, 109, 23), // "on_actionSave_triggered"
+QT_MOC_LITERAL(7, 133, 25), // "on_DrawRectButton_clicked"
+QT_MOC_LITERAL(8, 159, 29), // "on_DrawTriangleButton_clicked"
+QT_MOC_LITERAL(9, 189, 24), // "on_RotateLButton_clicked"
+QT_MOC_LITERAL(10, 214, 24), // "on_RotateRButton_clicked"
+QT_MOC_LITERAL(11, 239, 25), // "on_AddImageButton_clicked"
+QT_MOC_LITERAL(12, 265, 22), // "on_selectImage_clicked"
+QT_MOC_LITERAL(13, 288, 25), // "on_addClassButton_clicked"
+QT_MOC_LITERAL(14, 314, 33), // "on_newClassLineEdit_returnPre..."
+QT_MOC_LITERAL(15, 348, 34), // "on_sortClassBy_currentIndexCh..."
+QT_MOC_LITERAL(16, 383, 4), // "arg1"
+QT_MOC_LITERAL(17, 388, 34), // "on_sortImageBy_currentIndexCh..."
+QT_MOC_LITERAL(18, 423, 9), // "openImage"
+QT_MOC_LITERAL(19, 433, 9), // "imagePath"
+QT_MOC_LITERAL(20, 443, 17), // "showMousePosition"
+QT_MOC_LITERAL(21, 461, 7), // "QPoint&"
+QT_MOC_LITERAL(22, 469, 3), // "pos"
+QT_MOC_LITERAL(23, 473, 15), // "mousePressEvent"
+QT_MOC_LITERAL(24, 489, 12), // "QMouseEvent*"
+QT_MOC_LITERAL(25, 502, 11) // "mouse_event"
+
+    },
+    "MainWindow\0on_ZoomInButton_clicked\0\0"
+    "on_ZoomOutButton_clicked\0"
+    "on_actionOpen_triggered\0on_actionQuit_triggered\0"
+    "on_actionSave_triggered\0"
+    "on_DrawRectButton_clicked\0"
+    "on_DrawTriangleButton_clicked\0"
+    "on_RotateLButton_clicked\0"
+    "on_RotateRButton_clicked\0"
+    "on_AddImageButton_clicked\0"
+    "on_selectImage_clicked\0on_addClassButton_clicked\0"
+    "on_newClassLineEdit_returnPressed\0"
+    "on_sortClassBy_currentIndexChanged\0"
+    "arg1\0on_sortImageBy_currentIndexChanged\0"
+    "openImage\0imagePath\0showMousePosition\0"
+    "QPoint&\0pos\0mousePressEvent\0QMouseEvent*\0"
+    "mouse_event"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    0,  105,    2, 0x08 /* Private */,
+       4,    0,  106,    2, 0x08 /* Private */,
+       5,    0,  107,    2, 0x08 /* Private */,
+       6,    0,  108,    2, 0x08 /* Private */,
+       7,    0,  109,    2, 0x08 /* Private */,
+       8,    0,  110,    2, 0x08 /* Private */,
+       9,    0,  111,    2, 0x08 /* Private */,
+      10,    0,  112,    2, 0x08 /* Private */,
+      11,    0,  113,    2, 0x08 /* Private */,
+      12,    0,  114,    2, 0x08 /* Private */,
+      13,    0,  115,    2, 0x08 /* Private */,
+      14,    0,  116,    2, 0x08 /* Private */,
+      15,    1,  117,    2, 0x08 /* Private */,
+      17,    1,  120,    2, 0x08 /* Private */,
+      18,    1,  123,    2, 0x08 /* Private */,
+      20,    1,  126,    2, 0x0a /* Public */,
+      23,    1,  129,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   16,
+    QMetaType::Void, QMetaType::QString,   16,
+    QMetaType::Void, QMetaType::QString,   19,
+    QMetaType::Void, 0x80000000 | 21,   22,
+    QMetaType::Void, 0x80000000 | 24,   25,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_ZoomInButton_clicked(); break;
+        case 1: _t->on_ZoomOutButton_clicked(); break;
+        case 2: _t->on_actionOpen_triggered(); break;
+        case 3: _t->on_actionQuit_triggered(); break;
+        case 4: _t->on_actionSave_triggered(); break;
+        case 5: _t->on_DrawRectButton_clicked(); break;
+        case 6: _t->on_DrawTriangleButton_clicked(); break;
+        case 7: _t->on_RotateLButton_clicked(); break;
+        case 8: _t->on_RotateRButton_clicked(); break;
+        case 9: _t->on_AddImageButton_clicked(); break;
+        case 10: _t->on_selectImage_clicked(); break;
+        case 11: _t->on_addClassButton_clicked(); break;
+        case 12: _t->on_newClassLineEdit_returnPressed(); break;
+        case 13: _t->on_sortClassBy_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 14: _t->on_sortImageBy_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 15: _t->openImage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 16: _t->showMousePosition((*reinterpret_cast< QPoint(*)>(_a[1]))); break;
+        case 17: _t->mousePressEvent((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
